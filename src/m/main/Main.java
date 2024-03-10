989d55f18ecd7cf9396d852ae4025bc0aca4bdd8
sghd6 <142858898+sghd6@users.noreply.github.com>
2024-03-10 16:51:29 -0500

    Update Main.java
    Updated main class to reflect the example required by the assignment documentation.

--- src/m/main/Main.java
+++ src/m/main/Main.java
@@ -1,9 +1,14 @@
 package m.main;
 
 public class Main {
-
-	public Main() {
-		// TODO Auto-generated constructor stub
-	}
-
+	
+public static void main(String[] args) {
+	
+// Instantiate a pizzaOrder, perform operations based on the requirements.
+PizzaOrder order = new PizzaOrder();
+	
+// Adds pizzas to the cart, selects cooking strategies for the pizzas in the cart,
+prints pizza order cart. Calls checkout to calculate the bill, throws exception if
+triggered.
+// TODO
 }
