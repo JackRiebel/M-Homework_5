d39e6261b308a64bb372bc7c767196c531434f50
sghd6 <142858898+sghd6@users.noreply.github.com>
2024-03-10 16:39:26 -0500

    Update ConventionalOvenCookingStrategy.java
    ConventionalOvenCookingStrategy is now able to modify the price object pizza.

--- src/m/cookingStrategy/ConventionalOvenCookingStrategy.java
+++ src/m/cookingStrategy/ConventionalOvenCookingStrategy.java
@@ -1,9 +1,16 @@
 package m.cookingStrategy;
 
-public class ConventionalOvenCookingStrategy {
+public class ConventionalOvenCookingStrategy implements ICookingStrategy{
 
-	public ConventionalOvenCookingStrategy() {
-		// TODO Auto-generated constructor stub
+	private final double additional = 8;
+
+	@Override
+	public void cook(Pizza pizza) {
+		System.out.println("Conventional oven cooking method has been selected");
+		int price = pizza.getPrice();
+		pizza.setPrice(price + additional);
+		// TODO Auto-generated method stub
+		
 	}
 
 }
