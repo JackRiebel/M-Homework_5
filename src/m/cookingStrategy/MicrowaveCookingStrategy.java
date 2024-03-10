014ef654051d3ae48b9dee1301bdeb51623bd73e
sghd6 <142858898+sghd6@users.noreply.github.com>
2024-03-10 16:43:32 -0500

    Update MicrowaveCookingStrategy.java
    Microwave Cooking Method has been updated. It will now affect the price of object pizza.

--- src/m/cookingStrategy/MicrowaveCookingStrategy.java
+++ src/m/cookingStrategy/MicrowaveCookingStrategy.java
@@ -1,9 +1,16 @@
 package m.cookingStrategy;
 
-public class MicrowaveCookingStrategy {
+public class MicrowaveCookingStrategy implements ICookingStrategy{
+	
+	
+	private final double additional = 1;
+	
+	public void cook(Pizza pizza) {
+		System.out.println("Microwave cooking method has been selected");
+		int price = pizza.getPrice();
+		pizza.setPrice(price + additional);
+		// TODO Auto-generated method stub
 
-	public MicrowaveCookingStrategy() {
-		// TODO Auto-generated constructor stub
-	}
+}
 
 }
