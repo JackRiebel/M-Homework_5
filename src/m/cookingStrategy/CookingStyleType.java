8d961d7dff3ac9b80a62406738d2f9f53d46bdb6
GrantE2003 <116228933+GrantE2003@users.noreply.github.com>
2024-03-12 11:02:02 -0500

    Updated CookingStyleType.java
    Added the enum for pizza cooking styles and the associated prices.

--- src/m/cookingStrategy/CookingStyleType.java
+++ src/m/cookingStrategy/CookingStyleType.java
@@ -1,5 +1,17 @@
 package m.cookingStrategy;
 
 public enum CookingStyleType {
-
+	MICROWAVE(1.0),
+	CONVENTIONAL_OVEN(8.0),
+	BRICK_OVEN(10.0);
+	
+	private final double cookingStylePrice;
+	
+	CookingStyleType (double cookingStylePrice){
+		this.cookingStylePrice = cookingStylePrice;
+	}
+	
+	public double getCookingStylePrice() {
+		return cookingStylePrice;
+	}
 }
