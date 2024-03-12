68f0900135306398c30c6c1cfb956a4d70f0fe59
GrantE2003 <116228933+GrantE2003@users.noreply.github.com>
2024-03-12 12:00:32 -0500

    Updated PizzaCookingFactory.java
    Hopefully created the factory needed to create pizzas.

--- src/m/pizzaOrder/PizzaCookingFactory.java
+++ src/m/pizzaOrder/PizzaCookingFactory.java
@@ -1,9 +1,35 @@
 package m.pizzaOrder;
 
+import m.pizza.AbstractPizza;
+import m.pizza.HawaiianPizza;
+import m.pizza.MargheritaPizza;
+import m.pizza.SupremePizza;
+import m.pizza.VegetarianPizza;
+import m.pizza.PizzaType;
+
 public class PizzaCookingFactory {
-
-	public PizzaCookingFactory() {
-		// TODO Auto-generated constructor stub
+	
+	public AbstractPizza createPizza(PizzaType pizzaType) {
+		AbstractPizza pizza = null;
+		
+		switch (pizzaType) {
+			case HAWAIIAN:
+				pizza = new HawaiianPizza();
+				break;
+			case MARGHERITA:
+				pizza = new MargheritaPizza();
+				break;
+			case SUPREME:
+				pizza = new SupremePizza();
+				break;
+			case VEGETARIAN:
+				pizza = new VegetarianPizza();
+				break;
+			default:
+				throw new IllegalArgumentException("Unknown Pizza Type " + pizzaType);
+		}
+		pizza.setPizzaOrderID(AbstractPizza.getPizzaOrderID());
+		
+		return pizza;
 	}
-
 }
