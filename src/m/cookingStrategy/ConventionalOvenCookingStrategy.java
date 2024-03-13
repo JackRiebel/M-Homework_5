5952d80697bd0137937fb34849dd80707d13ba8c
sghd6 <142858898+sghd6@users.noreply.github.com>
2024-03-12 22:32:38 -0500

    Update ConventionalOvenCookingStrategy.java
    Conventional Oven Strategy now updates totalprice.

--- src/m/cookingStrategy/ConventionalOvenCookingStrategy.java
+++ src/m/cookingStrategy/ConventionalOvenCookingStrategy.java
@@ -1,16 +1,20 @@
 package m.cookingStrategy;
 
+import m.pizza.AbstractPizza;
+
 public class ConventionalOvenCookingStrategy implements ICookingStrategy{
 
 	private final double additional = 8;
 
 	@Override
-	public void cook(Pizza pizza) {
+	public void cook(AbstractPizza pizza) 
+	{
 		System.out.println("Conventional oven cooking method has been selected");
-		int price = pizza.getPrice();
-		pizza.setPrice(price + additional);
+		double price = pizza.getTotalPrice();
+		pizza.setTotalPrice(price + additional);
 		// TODO Auto-generated method stub
 		
 	}
 
 }
+
