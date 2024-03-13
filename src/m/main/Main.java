efa1328ac4563233e296ac5cdc057da1470124df
sghd6 <142858898+sghd6@users.noreply.github.com>
2024-03-12 22:50:43 -0500

    Update Main.java
    Fixed a comment issue in main function.

--- src/m/main/Main.java
+++ src/m/main/Main.java
@@ -8,7 +8,7 @@ public static void main(String[] args) {
 PizzaOrder order = new PizzaOrder();
 	
 // Adds pizzas to the cart, selects cooking strategies for the pizzas in the cart,
-prints pizza order cart. Calls checkout to calculate the bill, throws exception if
-triggered.
+//prints pizza order cart. Calls checkout to calculate the bill, throws exception if
+//triggered.
 // TODO
 }
